ae39ecafd7c1b1a2bdb30dec0a23f2c5fbb0b2c2
Wei Yao <s1cca@outlook.com>
2023-05-10 16:20:38 +0100

    final version

--- May.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter a number: "))  # Ask user for a number
-
-print("Odd numbers up to", num, ":")  # Print a message indicating the odd numbers will be listed
-
-for i in range(num + 1):  # Loop through numbers up to user's input
-    if i % 2 != 0:  # Check if number is odd
-        print(i)  # If odd, print the number
